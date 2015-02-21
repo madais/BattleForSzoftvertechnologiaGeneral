e9e7b274c742fb98d5d4db36b08278022fa51ccb
efhenrir <efhenrir@gmail.com>
2015-02-21 12:10:39 +0100

    Commit messages are important and cool... like sloths and bowties.

--- BattleForSzoftvertechnologiaGeneral/src/BattleForSzoftvertechnologiaGeneral.java
+++ BattleForSzoftvertechnologiaGeneral/src/BattleForSzoftvertechnologiaGeneral.java
@@ -1,11 +1,36 @@
+import javax.swing.JOptionPane;
+
 
 public class BattleForSzoftvertechnologiaGeneral {
 
 	public static void main(String[] args) {
-		
-
+// Commit test:		
+//		Király lesz, annyit érdemes még egyeztetnünk, hogy a fájlok milyen
+//		encoding szerint legyenek, mert windowson nem teljesen olvashatóak a linux
+//		alatt írt spec/ékezetes karakterek. Zoli
 		System.out.println("Ez lesz a szuper jÃ¡tÃ©k de tÃ©nyleg!");
 		System.out.println("( Í¡Â° ÍœÊ– Í¡Â°)");
+
+
+//                      ^    ^
+//                     / \  //\
+//       |\___/|      /   \//  .\
+//       /O  O  \__  /    //  | \ \
+//      /     /  \/_/    //   |  \  \
+//      @___@'    \/_   //    |   \   \ 
+//         |       \/_ //     |    \    \ 
+//         |        \///      |     \     \ 
+//        _|_ /   )  //       |      \     _\
+//       '/,_ _ _/  ( ; -.    |    _ _\.-~        .-~~~^-.
+//       ,-{        _      `-.|.-~-.           .~         `.
+//       '/\      /                 ~-. _ .-~      .-~^-.  \
+//          `.   {            }                   /      \  \
+//        .----~-.\        \-'                 .~         \  `. \^-.
+//       ///.----..>    c   \             _ -~             `.  ^-`   ^-_
+//         ///-._ _ _ _ _ _ _}^ - - - - ~                     ~--,   .-~
+
+		
+		JOptionPane.showMessageDialog(null, "A buddhista zen televízió férfi bemondónõje vagyok. A Grzegorz Brzêczyszczykiewicz emlékcsoportról tudósítunk. ");
 	}
 
 }
