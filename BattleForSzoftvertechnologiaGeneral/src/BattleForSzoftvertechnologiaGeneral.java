dc4d22a4473f2d5c89636111b0d8401210a578ac
fhenrir <efhenrir@gmail.com>
2015-05-09 18:30:28 +0200

    fostalicska git

--- BattleForSzoftvertechnologiaGeneral/src/BattleForSzoftvertechnologiaGeneral.java
+++ BattleForSzoftvertechnologiaGeneral/src/BattleForSzoftvertechnologiaGeneral.java
@@ -61,12 +61,12 @@ public class BattleForSzoftvertechnologiaGeneral {
 			ex.printStackTrace();
 		}*/
 		
-		try{
-			CommTest.main(args);
-		}
-		catch (IOException ex){
-			ex.printStackTrace();
-		}
+//		try{
+//			CommTest.main(args);
+//		}
+//		catch (IOException ex){
+//			ex.printStackTrace();
+//		}
 	}
 
 }
